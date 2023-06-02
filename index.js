5b9029e885e1d83921e712debae8309f827eb6c7
Anthony <arm432525@live.com>
2023-06-02 17:34:42 -0400

    DOM nodes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+    const newHeader = document.createElement("h1");
+    newHeader.id = "victory";
+    newHeader.textContent = "Anthony is the champion";
+    document.body.appendChild(newHeader);
+
+
+
+  // Write your code here!
